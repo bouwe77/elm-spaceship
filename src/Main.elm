2ee14f7b7eeacba31cddc59848f59a966b7037ad
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2023-03-10 22:24:54 +0100

    feat: getting a spaceship from the API through HTTP and display the name

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,117 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as D exposing (Decoder)
 
 
 
----- MODEL ----
+-- MAIN
 
 
-type alias Model =
-    {}
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success Spaceship
+
+
+type alias Spaceship =
+  { name : String
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Loading, getSpaceship)
 
 
 
----- UPDATE ----
+-- UPDATE
 
 
 type Msg
-    = NoOp
+  = GotSpaceship (Result Http.Error Spaceship)
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    ( model, Cmd.none )
+  case msg of
+    GotSpaceship result ->
+      case result of
+        Ok spaceship ->
+          (Success spaceship, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
 
 
 
----- VIEW ----
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ h1 [] [ text "Your Elm App is working!" ]
+  div []
+    [ h1 [] [ text "My Spaceship" ]
+    , viewSpaceship model
+    ]
+
+
+viewSpaceship : Model -> Html Msg
+viewSpaceship model =
+  case model of
+    Failure ->
+      div []
+        [ text "Loading the spaceship failed..."
+        ]
+
+    Loading ->
+      text "Loading..."
+
+    Success spaceship ->
+      div []
+        [ h2 [] [ text spaceship.name ]
         ]
 
 
 
----- PROGRAM ----
+-- HTTP
 
 
-main : Program () Model Msg
-main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+getSpaceship : Cmd Msg
+getSpaceship =
+  Http.get
+    { url = "http://localhost:55667/spaceships/bouwe"
+    , expect = Http.expectJson GotSpaceship spaceshipDecoder
+    }
+
+
+spaceshipDecoder : Decoder Spaceship
+spaceshipDecoder =
+  D.map Spaceship
+    (D.field "name" D.string)
